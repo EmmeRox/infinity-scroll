2ecc398c60e615f8272ae629c5005a8a6811ce95
EmmeRox <99152085+EmmeRox@users.noreply.github.com>
2023-02-16 15:06:03 -0600

    added loading animation to initial load

--- script.js
+++ script.js
@@ -2,15 +2,23 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
-const count = 10;
+let count = 5;
 const apiKey = "c_HNVEGfgMEp0tn9YF5-BlQIxlNYNmCSlYQHOUeiuog";
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
 function imageLoaded() {
-  console.log("image loaded");
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    count = 30;
+  }
 }
 
 // Helper function to set attributes on DOM elements
@@ -22,6 +30,8 @@ function setAttributes(element, attributes) {
 
 //Create elements for links
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
   // Run functin for each object in array
   photosArray.forEach((photo) => {
     // Create <a> to link to unsplash
@@ -65,11 +75,11 @@ async function getPhotos() {
 //Check to see if scrolling near bottom
 window.addEventListener("scroll", () => {
   if (
-    window.innerHeight + window.scrollY >=
-    document.body.offsetHeight - 1000
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
   ) {
     getPhotos();
-    console.log("load");
+    ready = false;
   }
 });
 
